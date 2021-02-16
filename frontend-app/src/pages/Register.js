82edd8982ca55b82357fd5618060a4b36f977a5c
mert esen <mertesen3@gmail.com>
2021-02-16 16:43:40 +0300

    changed backend url

--- frontend-app/src/pages/Register.js
+++ frontend-app/src/pages/Register.js
@@ -19,7 +19,7 @@ export default function Register() {
 
     const register = (e) => {
         e.preventDefault();
-        fetch("http://localhost:8080/api/user/register",{
+        fetch("https://spring-react-mysql-todo-app.herokuapp.com/api/user/register",{
             method:"POST",
             headers: {
                 "Content-Type":"application/json",
